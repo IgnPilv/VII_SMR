c811fb5e7e808b3b77280978933f7b3a26d1d9b5
IgnPilv <37305298+IgnPilv@users.noreply.github.com>
2018-03-14 23:00:43 +0000

    copy from shortcut

---
+++ ODEsolver.m
@@ -0,0 +1,129 @@
+% ----------------------------------------------------------------------- %
+% ODEsolver.m
+% Cumene production packed bed reactor ODE solver
+% ----------------------------------------------------------------------- %
+%
+% Malcolm Bell, Sophie Fitchett, Adam Machon, Jack Palmer, Ignas Pilvinis
+% S1433709 S1405595 S1426444 S1439172 S1452897
+% 07/11/2017
+% ----------------------------------------------------------------------- %
+% SingleReactorPlotter.m
+% MultipleReactorOptimizer.m
+% calcz.m
+% ODEsolver.m
+% ----------------------------------------------------------------------- %
+function dYfuncvecdW = ODEsolver(W,Yfuncvec);
+Fa = Yfuncvec(1); % kmol/h ch4 flow rate
+Fb = Yfuncvec(2); % kmol/h h2o flow rate
+Fc = Yfuncvec(3); % kmol/h co flow rate
+Fd = Yfuncvec(4); % kmol/h h2 flow rate
+Fe = Yfuncvec(5); % kmol/h co2 flow rate
+P = Yfuncvec(6); % Pa
+T = Yfuncvec(7); % K Temperature
+Ta = Yfuncvec(8); % K Coolant Temperature
+Qr = Yfuncvec(9); % W Heat generated
+Qf = Yfuncvec(10); % W Heat generated
+Tw = Yfuncvec(11); % W Heat generated
+global P0 T0 Ft0 rhoc U a Rgas alpha rlist glbp Wspan2 rhob Ac sb hi
+% Explicit equations
+Ft = Fa + Fb + Fc + Fd + Fe; % kmol/h total molar flowrate
+% Heat of reactions
+%Shotmate equation parameters for CH4 H2O CO H2 CO2
+coeffA = [-0.703029,30.092,25.56759,33.066178,24.99735];
+coeffB = [108.4773,6.832514,6.09613,-11.363417,55.18696];
+coeffC = [-42.52157,6.793435,4.054656,11.432816,-33.69137];
+coeffD = [5.862788,-2.53448,-2.671301,-2.772874,7.948387];
+coeffE = [0.678565,0.082139,0.131021,-0.158558,-0.136638];
+coeffF = [-76.84376,-250.881,-118.0089,-9.980797,-403.6075];
+coeffG = [158.7163,223.3967,227.3665,172.707974,228.2431];
+coeffH = [-74.8731,-241.8264,-110.5271,0,-393.5224];
+
+t1 = ((T)/1000); % 1/K 827C or 1100K temperature for mean capacity
+Cpbank = coeffA + coeffB.*t1 + coeffC.*t1^2 + coeffD.*t1^3 + coeffE./t1^2; % J/(mol K) heat capacities
+Cpa = Cpbank(1); % J/(mol K) heat capacity
+Cpb = Cpbank(2); % J/(mol K) heat capacity
+Cpc = Cpbank(3); % J/(mol K) heat capacity
+Cpd = Cpbank(4); % J/(mol K) heat capacity
+Cpe = Cpbank(5); % J/(mol K) heat capacity
+
+dCp1std = 46.4252; % J/mol K basis h2o
+dCp2std = 3.22087; % J/mol K basis h2o
+dCp3std = 49.6461; % J/mol K basis ch4
+
+dCp1 = Cpc + 3*Cpd - Cpb - Cpa; % J/mol K basis h2o
+dCp2 = Cpd + Cpe - Cpc - Cpb; % J/mol K basis h2o
+dCp3 = 4*Cpd + Cpe - 2*Cpb - Cpa; % J/mol K basis ch4
+
+Hrx1 = 205700; % J/mol at 25 basis h2o
+Hrx2 = -41550; % J/mol at 25 basis h2o
+Hrx3 = 164150; % J/mol at 25 basis ch4
+
+dHrx1 = Hrx1 + (dCp1+dCp1std)/2*(T-(298.15)); % J/mol Heat of reaction (reference 25degC)
+dHrx2 = Hrx2 + (dCp2+dCp2std)/2*(T-(298.15)); % J/mol Heat of reaction (reference 25degC)
+dHrx3 = Hrx3 + (dCp3+dCp3std)/2*(T-(298.15)); % J/mol Heat of reaction (reference 25degC)
+
+% Reaction rate constants
+k1 = glbp(1)*4.22*10^15 * exp(-240100/(Rgas.*T)); % rate coefficients [kmol bar0.5 / kgcat h]
+k2 = glbp(2)*1955000 * exp(-67130/(Rgas.*T)); % rate coefficients [kmol / kgcat h]
+k3 = glbp(3)*1.02*10^15 * exp(-243900/(Rgas.*T)); % rate coefficients [kmol bar0.5 / kgcat h]
+
+% Reaction equilibrium constants
+
+K1 = exp(30.42-27106./T); % reaction rate constant [bar2]
+K2 = exp(-3.798+4160./T); % reaction rate constant -
+% K3 = exp(34.218-31266./T); % reaction rate constant [bar2]
+K3 = K1.*K2;
+
+% Adsorbtion constants
+Ka = 6.65*10^-4*exp(38280/(Rgas.*T)); % ch4 bar-1
+Kb = 6.12*10^-9*exp(82900/(Rgas.*T)); % h2o - 
+Kc = 8.23*10^-5*exp(70650/(Rgas.*T)); % co bar-1
+Kd = 1.77*10^5*exp(-88680/(Rgas.*T)); % h2 bar-1
+% Ke = 1; % co2 -
+
+% Stoichiometry
+% Cto = P0./(Rsi*T0*1000*z); % mol/L initial feed concentration
+pch4 = (Fa / Ft) * (P/100000); %/P0) * (T0/T); % bar ch4 concentration bar
+ph2o = (Fb / Ft) * (P/100000); %/P0) * (T0/T); % bar h2o concentration bar
+pco = (Fc / Ft) * (P/100000); %/P0) * (T0/T); % bar co concentration bar
+ph2 = (Fd / Ft) * (P/100000); %/P0) * (T0/T); % bar h2 concentration bar
+pco2 = (Fe / Ft) * (P/100000); %/P0) * (T0/T); % bar co2 concentration bar
+
+% Rate laws
+den = (1 + Kc*pco + Kd*ph2 + Ka*pch4 + Kb*ph2o/ph2); % denominator term
+r1 = ((k1/(ph2^2.5))*(pch4*ph2o-((ph2^3)*pco/K1)))/((den)^2); % first reaction [kmol/kgcat/h]
+r2 = ((k2/ph2)*(pco*ph2o-(ph2*pco2/K2)))/((den)^2); % second reaction [kmol/kgcat/h]
+r3 = ((k3/(ph2^3.5))*(pch4*(ph2o^2)-((ph2^4)*pco2/K3)))/((den)^2); % third reaction [kmol/kgcat/h]
+
+
+Wspan2 = [Wspan2 W];
+rlist = [rlist [W r1 r2 r3]'];
+
+% Differential equations / Mole Balance
+dFadW = -r1-r3; %[kmol/kgcat/h]
+dFbdW = -r1-r2-2*r3;
+dFcdW = r1-r2;
+dFddW = 3*r1+r2+4*r3;
+dFedW = r2+r3;
+dPdW = glbp(6)*(-alpha/2)*(P0/(P/P0))*(T/T0)*(Ft/Ft0);
+
+
+% dTdW = ((0.75*U*3600/rhob * a * (Tw - T)) + glbp(5)*(-1*r1*1000 * dHrx1 - r2*1000 * dHrx2 - r3*1000 * dHrx3))/(Fa*1000*Cpa + Fb*1000*Cpb + Fc*1000*Cpc + Fd*1000*Cpd + Fe*1000*Cpe);
+% dTadW = 0;
+% dQrdW = -1*(-r1*1000 * dHrx1 - r2*1000 * dHrx2 - r3*1000 * dHrx3);
+% dQhdW = (sb*3600/rhob * a * (Ta.^4 - Tw.^4));
+% dTwdW =  ((sb*3600/rhob * a * (Ta.^4 - Tw.^4)) - (0.75*U*3600/rhob * a * (Tw - T)))/(Fa*1000*Cpa + Fb*1000*Cpb + Fc*1000*Cpc + Fd*1000*Cpd + Fe*1000*Cpe);
+
+
+dTdW = ((U*3600/rhob * a * (Ta - T)) + glbp(5)*(-1*r1*1000 * dHrx1 - r2*1000 * dHrx2 - r3*1000 * dHrx3))/(Fa*1000*Cpa + Fb*1000*Cpb + Fc*1000*Cpc + Fd*1000*Cpd + Fe*1000*Cpe);
+dTadW = 0;
+dQrdW = -1*(-r1*1000 * dHrx1 - r2*1000 * dHrx2 - r3*1000 * dHrx3);
+dQhdW = (U*3600/rhob * a * (Ta - T));
+dTwdW = 0*((sb*3600*a*(Ta.^4-Tw.^4)/(rhob))-(U*3600/rhob * a * (Tw - T)))/(Fa*1000*Cpa + Fb*1000*Cpb + Fc*1000*Cpc + Fd*1000*Cpd + Fe*1000*Cpe);;
+
+% dTdW = ((glbp(4)*U*3600/rhoc * a * (Ta - T)) + glbp(5)*(-1*r1*1000 * dHrx1 - r2*1000 * dHrx2 - r3*1000 * dHrx3))/(Fa*1000*Cpa + Fb*1000*Cpb + Fc*1000*Cpc + Fd*1000*Cpd + Fe*1000*Cpe);
+% dTdW = ((-1*glbp(4)*U*3600*a*(T-Ta)/(rhob)) + glbp(5)*(-1*r1*1000 * dHrx1 - r2*1000 * dHrx2 - r3*1000 * dHrx3))/(Fa*1000*Cpa + Fb*1000*Cpb + Fc*1000*Cpc + Fd*1000*Cpd + Fe*1000*Cpe);
+% dTadW = 0;
+% dQrdW = (-r1*1000 * dHrx1 - r2*1000 * dHrx2 - r3*1000 * dHrx3);
+% dQhdW = (U*3600/rhob * a * (Ta - T));
+dYfuncvecdW = [dFadW; dFbdW; dFcdW; dFddW; dFedW; dPdW; dTdW;dTadW;dQrdW;dQhdW;dTwdW];
---
+++ SingleReactorPlotter.m
@@ -0,0 +1,183 @@
+% ----------------------------------------------------------------------- %
+% SingleReactorPlotter.m
+% ----------------------------------------------------------------------- %
+% SingleReactorPlotter.m
+% MultipleReactorOptimizer.m
+% calcz.m
+% ODEsolver.m
+% ----------------------------------------------------------------------- %
+close all
+clear all
+clc
+global P0 T0 Ft0 rhoc U a Rgas alpha rlist glbp Wspan2 rhob Ac sb hi
+% -- DECLARATIONS OF INPUT PARAMETERS
+% Wspan = linspace(0,20000,50000); % Range for the weight of the catalyst kg (xaxis of produced graphs)
+Wspan = (0:5:60000); % Range for the weight of the catalyst kg (xaxis of produced graphs)
+
+rlist = [];
+
+% Simulation options [0/1]
+% r1,r2,r3,heating,reactionheat,dP, inletflow scale
+glbp = [1 0 0 1 1 1 1];
+
+
+P0 = 20*10^5; % Pa inlet pressure
+T0 = 500+273.15; % K inlet temperature
+Ta0 = 1200+273.15; % K inlet coolant temperature
+% a-ch4 b-h2o c-co d-h2 e-co2
+% Fa0 = glbp(7)*1292.0; % kmol/h inlet ch4 flow rate
+% Fb0 = glbp(7)*5350.4; % kmol/h inlet h2o flow rate 3226
+% Fc0 = glbp(7)*149.60; % kmol/h inlet co flow rate
+% Fd0 = glbp(7)*367.20; % kmol/h inlet h2 flow rate
+% Fe0 = glbp(7)*1; % kmol/h inlet co2 flow rate
+
+Fa0 = glbp(7)*1292.0; % kmol/h inlet ch4 flow rate
+Fb0 = glbp(7)*5350.4; % kmol/h inlet h2o flow rate 3226
+Fc0 = glbp(7)*149.60; % kmol/h inlet co flow rate
+Fd0 = glbp(7)*367.2; % kmol/h inlet h2 flow rate
+Fe0 = glbp(7)*1; % kmol/h inlet co2 flow rate
+
+Ft0 = Fa0 + Fb0 + Fc0 + Fd0 + Fe0; % kmol/h inlet total flow rate
+% z = 1; % ideal gas for now
+Rgas = 8.3144598; % kJ/kmol
+sb = 5.670367*10^-8; % W?m?2?K?4
+% --Tube Data
+Dt = 0.0762; % m tube diameter
+% Dt = 0.020; % m tube diameter
+
+Nt = 1200; % number of tubes (used for pressure drop)
+Ac = 3.1415*Dt*Dt/4*Nt; % m² tube x-section area (used for pressuredrop)
+a = 4/Dt; % 1/m area to volume ratio
+
+% --Furnace data
+U = 100; % W/(m² K) overall heat transfer coefficient
+
+
+% --Catalyst data
+e = 0.528; % - voidage
+rhoc = 930; % kg/m³ particle density
+rhob = rhoc*(1-e);
+Dp = 0.025; % m particle diameter
+
+% % -- PRESSURE DROP CALCULATIONS
+m = (Fa0*16.04+Fb0*18+Fc0*28.01+Fd0*2+Fe0*44)/3600; % kg/s Feed mass flow (used for pressure drop)
+nu = 0.000027915; % kg/(m s) viscosity) Average value retrieved FROM THERMOWORKBENCH (used for pressure drop)
+rho0 = P0*(m/(Ft0*1000/3600))/(T0*Rgas); % kg/m³ (used for pressure drop)
+% 3.2533 kg/m³
+G = m/(Ac); % kg/(m² s) mass flux (total mass / total area) (used for pressure drop)
+Beta0 = (G/(rho0*Dp)) * ((1-e)/(e*e*e)) * (((150*(1-e)*nu)/Dp)+(1.75*G)); % (used for pressure drop)
+alpha = (2*Beta0)/(Ac*rhob*P0); % (used for pressure drop)
+u = m/rho0/Ac; %m/;s
+
+% k = 0.1422; %W/m K
+% Re = G*Dt/nu; % Reynolds
+% Cpcorr = 700.743; %kJ/kg K
+% Pr = Cpcorr*nu/k; %Prandtl
+% Nu = 0.023*Re^0.8*Pr^0.4;
+% hi = Nu*k/Dt
+
+% % -- INPUT PARAMETERS FOR ODE SOLVER
+i0 = [Fa0; Fb0; Fc0; Fd0; Fe0; P0; T0; Ta0; 0; 0; T0;]; % Initial values for the dependent variables.
+% % -- CALL TO THE SOLVER FUNCTION
+[w0,y0]=ode45(@ODEsolver,Wspan,i0); % Range of solved values for a range of catalyst mass stored in this array
+% % -- INTERPOLATION OF OUTPUT DATA
+% disp(y0(:,1));
+Wf = interp1q(1-y0(:,1)./Fa0,w0,0.975) % kg needed mass of catalyst for 4750kmol/h of H2
+% Wf = interp1q(y0(:,1)./Fa0,w0,50.0) % kg needed mass of catalyst for 4750kmol/h of H2
+Tf = interp1(w0,y0(:,7),Wf)-273.15; % C outlet temperature
+Taf = interp1(w0,y0(:,8),Wf)-273.15; % C outlet coolant temperature
+Pf = interp1(w0,y0(:,6),Wf); % Pa outlet pressure
+Lf = Wf/(Ac*(1-e)*rhoc); % m Length of reactor
+conv = 1-interp1(w0,y0(:,1),Wf)./Fa0; % - conversation of methane
+Faf = interp1(w0,y0(:,1),Wf); % kmol/h outlet CH4
+Fbf = interp1(w0,y0(:,2),Wf); % kmol/h outlet H2O
+Fcf = interp1(w0,y0(:,3),Wf); % kmol/h outlet CO
+Fdf = interp1(w0,y0(:,4),Wf); % kmol/h outlet H2
+Fef = interp1(w0,y0(:,5),Wf); % kmol/h outlet CO2
+Vf = Wf /(rhoc*(1-e)); % m³ total volume of reactor
+y0(:,9)=y0(:,9)./3600*10^-6;
+y0(:,10)=y0(:,10)./3600*10^-6;
+Qf = interp1(w0,y0(:,9),Wf) % total heat generated J
+Qff = interp1(w0,y0(:,10),Wf) % total heat generated J
+dPbar = (P0-Pf)/100000; % bar pressure drop
+Ftf = interp1(w0,y0(:,1),Wf)+interp1(w0,y0(:,2),Wf)+interp1(w0,y0(:,3),Wf)+interp1(w0,y0(:,4),Wf)+interp1(w0,y0(:,5),Wf);
+% -- TABULAR RESULTS
+fprintf(['INPUT:\nNumber of tubes: %i. Tube diameter: %1.3f m. Inlet temperature: %1.3f C. Inletcoolant temperature : %1.3f C. Inlet pressure : %1.3f Pa.\n'],Nt,Dt,T0-273,Ta0-273,P0 );
+
+
+fprintf(['CH4 feed: %1.3f kmol/h. H2O feed: %1.3f kmol/h. CO feed:: %1.3f kmol/h.\n'],Fa0,Fb0,Fc0);
+fprintf(['H2 feed: %1.3f kmol/h. CO2 feed: %1.3f kmol/h.\n'],Fd0,Fe0);
+fprintf(['OUTPUT:\nCatalyst mass : %1.3f kg. Outlet Temperature : %1.3f C. Coolant Outlet Temperature : %1.3f C. \nOutlet Pressure : %1.3f Pa. Length : %1.3f m. Conversion : %1.3f. Volume : %1.3f m³.\nCH4: %1.3f. H2O: %1.3f. CO: %1.3f. H2: %1.3f. CO2: %1.3f. kmol/h\n'],Wf,Tf,Taf,Pf,Lf,conv,Vf,Faf,Fbf,Fcf,Fdf,Fef);
+% T = table(Nt,T0-273,P0,Ta0-273,Fb0,Fp0,Wf,Tf,Pf,Taf,Lf,conv,V,dPbar); % Generate a table of results
+% T.Properties.VariableNames = {'Nt' 'T0' 'P0' 'Ta0' 'Fb0' 'Fp0' 'W' 'Tf' 'Pf' 'Taf' 'L' 'X' 'Vf' 'dPf'}
+
+% L0 = w0./(Nt*Ac*(1-e)*rhoc); % Length of the reactor
+% -- PLOTTER
+
+test = [Wspan',w0,y0,1-(y0(:,1))./Fa0];
+
+fig1 = figure;
+graph1 = plot(w0,(y0(:,1)),w0,(y0(:,2)),w0,(y0(:,3)),w0,(y0(:,4)),w0,(y0(:,5)));
+legend('CH4 kmol/h','H2O kmol/h','CO kmol/h','H2 kmol/h','CO2 kmol/h')
+xlabel('W, kg')
+% xlabel('L, m')
+ylabel('Flow, kmol/h')
+title('Reactor flow rate Profiles')
+
+fig2 = figure;
+hold all;
+dummy1 =(y0(:,1))+(y0(:,2))+(y0(:,3))+(y0(:,4))+(y0(:,5));
+graph2 = plot(w0,(y0(:,1))./dummy1,w0,(y0(:,2))./dummy1,w0,(y0(:,3))./dummy1,w0,(y0(:,4))./dummy1,w0,(y0(:,5))./dummy1);
+legend('CH4','H2O','CO','H2','CO2')
+xlabel('W, kg')
+ylabel('Mole Fraction')
+title('Reactor flow rate Profiles')
+
+fig3 = figure;
+hold all;
+graph3 = plot(w0,(y0(:,10)),w0,(y0(:,9)),w0,-(y0(:,9))+(y0(:,10)));
+legend('Furnace','Reaction','Sensible')
+xlabel('W, kg')
+ylabel('Q, MW')
+title('Heat consumption')
+
+fig4 = figure;
+hold all;
+graph4 = plot(w0,1-(y0(:,1))./Fa0);
+legend('CH4')
+xlabel('W, kg')
+ylabel('Mole Fraction')
+title('Methane Conversion')
+
+fig5 = figure;
+hold all;
+graph5 = plot(w0,(y0(:,7))-273,w0,(y0(:,8))-273,w0,(y0(:,11))-273);
+legend('Reactor','Furnace','Wall')
+xlabel('W, kg')
+ylabel('Temperature, C')
+title('Temperature Profile')
+
+fig6 = figure;
+hold all;
+graph6 = plot(w0,(y0(:,6))/100000);
+legend('Reactor')
+xlabel('W, kg')
+ylabel('Pressure, bar')
+title('Pressure Profile')
+
+set(fig1,'units','normalized','outerposition',[0 0.5 .333 .5])
+set(fig2,'units','normalized','outerposition',[0 0 .333 .5])
+set(fig3,'units','normalized','outerposition',[0.333 0 .333 .5])
+set(fig4,'units','normalized','outerposition',[0.333 0.5 .333 .5])
+set(fig5,'units','normalized','outerposition',[0.666 0.5 .333 .5])
+set(fig6,'units','normalized','outerposition',[0.666 0 .333 .5])
+rlist=rlist';
+% % --Display reaction rates
+% % fprintf([repmat('%7.4e    ',1,size(rlist,2)),'\n'],rlist')
+% % L0 = w0./(Ac*(1-e)*rhoc) % Length of the reactor
+% disp(1-(y0(1:10,1)./Fa0));
+avgQ = Qff/(Nt*Dt*3.14*Lf)
+% Twf = interp1(w0,y0(:,11),Wf)-273.15 % C outlet coolant temperature
+acpF = Qff/1;
+Atubes = Nt*Dt*3.14*Lf
+Acp15 = Nt*Dt*1.5*Lf
